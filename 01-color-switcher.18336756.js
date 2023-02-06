b1787b66514e7c82d4c4746024720682c5d4ca9f
BarybinaTetiana <109151353+BarybinaTetiana@users.noreply.github.com>
2023-02-06 17:54:17 +0000

    Deploying to gh-pages from @ BarybinaTetiana/goit-js-hw-09@206f67c45e29172e6a85db6331084c7027411b52 🚀

---
+++ 01-color-switcher.18336756.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.body;t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,a=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(a)}));var a=null}();
+//# sourceMappingURL=01-color-switcher.18336756.js.map
