206f67c45e29172e6a85db6331084c7027411b52
BarybinaTetiana <109151353+BarybinaTetiana@users.noreply.github.com>
2023-02-06 19:53:49 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyColor = document.body;
 
+startBtn.addEventListener('click', onStartColor);
+stopBtn.addEventListener('click', onStopColor);
+
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStartColor() {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  timerId = setInterval(() => {
+    bodyColor.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopColor() {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  clearInterval(timerId);
+}
